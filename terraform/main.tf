15f0813b35ccd15be40c5e4226fb5868e2d1e53c
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2019-07-11 17:18:12 +0300

    add main task terraform

---
+++ terraform/main.tf
@@ -0,0 +1,75 @@
+terraform {
+  # Версия terraform  
+  required_version = "0.11.11"
+}
+
+provider "google" {
+  # Версияпровайдера  
+  version = "2.0.0"
+
+  # ID проекта  
+  project = "${var.project}"
+  region  = "${var.region}"
+}
+
+resource "google_compute_instance" "app" {
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "${var.app_zone}"
+  tags         = ["reddit-app"]
+
+  # определениезагрузочногодиска 
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  # определениесетевогоинтерфейса
+  network_interface {
+    # сеть, ккоторойприсоединитьданныйинтерфейс    
+    network = "default"
+
+    # использовать ephemeral IP длядоступаизИнтернет    
+    access_config {}
+  }
+
+  metadata {
+    ssh-keys = "masterplan:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "masterplan"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+
+  # Названиесети, вкоторойдействуетправило  
+  network = "default"
+
+  # Какойдоступразрешить  
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  # Какимадресамразрешаемдоступ  
+  source_ranges = ["0.0.0.0/0"]
+
+  # Правилоприменимодляинстансовсперечисленнымитэгами  
+  target_tags = ["reddit-app"]
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,28 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+
+  # Значениепоумолчанию  
+  default = "europe-west1"
+}
+
+variable public_key_path {
+  # Описаниепеременной
+  description = "Path to the public key used for ssh access"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
+
+variable private_key_path {
+  description = "Path to private key"
+}
+
+variable app_zone {
+  description = "zone for app"
+  default     = "europe-west1-b"
+}
